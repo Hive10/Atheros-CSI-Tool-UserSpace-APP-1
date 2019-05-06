0449f3625a2b611b5d6300c3c98bc9bedab7c945
Kalvik Jakkala <Kd.kalvik@gmail.com>
2019-05-06 18:25:14 -0400

    Update start_hostapd.sh

--- scripts/start_hostapd.sh
+++ scripts/start_hostapd.sh
@@ -13,4 +13,7 @@ sleep 1
 sudo systemctl start hostapd.service
 
 #sudo systemctl unmask hostapd.service
+#sudo nano /etc/default/hostapd
+#DAEMON_CONF="/usr/local/sbin/Atheros-CSI-Tool-UserSpace-APP/scripts/hostapd.conf"
+
 #sudo nmcli con add con-name "neuralwave" ifname wlan0 type wifi ip4 192.168.1.103/24 ssid neuralwave
